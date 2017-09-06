c46837821880cc585fe2790892634244c36cf35b
Michael L Perry <michael@qedcode.com>
2017-09-05 20:51:59 -0500

    Created test project.

---
+++ Festify.UnitTest/Speakers/SpeakerTestContext.cs
@@ -0,0 +1,22 @@
+﻿using Festify.DAL.Speakers;
+using System;
+using System.Collections.Generic;
+
+namespace Festify.UnitTest.Speakers
+{
+    public class SpeakerTestContext
+    {
+        public SpeakerTestContext()
+        {
+        }
+        public IEnumerable<Speaker> WhenGetAllSpeakers()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static SpeakerTestContext GivenSpeakerRepository()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Festify.UnitTest/Speakers/SpeakerTests.cs
@@ -0,0 +1,22 @@
+﻿using Festify.DAL.Speakers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Festify.UnitTest.Speakers
+{
+    public class SpeakerTests
+    {
+        [Fact]
+        public void InitiallyNoSpeakers()
+        {
+            var context = SpeakerTestContext.GivenSpeakerRepository();
+            var speakers = context.WhenGetAllSpeakers();
+            speakers.Count().Should().Be(0);
+        }
+    }
+}
